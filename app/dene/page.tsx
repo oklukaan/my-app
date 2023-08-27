8e919ed33ab49745359821a6d41a107ec9c83f8d
oklukaan <kaan.oklu@hotmail.com>
2023-08-27 19:50:00 +0300

    qwe

--- app/dene/page.tsx
+++ app/dene/page.tsx
@@ -1,8 +1,10 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 async function Page({ params }: { params?: { id: string } }) {
-  return <span>My page</span>;
+  return (
+    <>
+    </>
+  )
 }
-
 // @ts-ignore
-export default withPageAuthRequired(Page, { returnTo: "/" });
+export default withPageAuthRequired(Page);
